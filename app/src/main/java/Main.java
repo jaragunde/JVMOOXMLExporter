9b4d2216f5189ff5c23a1fdcb8931cb1f1d5b2db
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2025-04-09 09:49:42 +0200

    Read output file name from command line.

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -4,12 +4,20 @@ import com.igalia.ooxmlexporter.DocumentConverterFactory;
 public class Main {
     public static void main (String[] args) {
         if (args.length == 0) {
-            System.out.println("Input file required as argument.");
+            System.out.println("""
+                    Missing input file.
+                    Arguments:
+                      input-file   mandatory, path to file to be converted.
+                      output-file  optional, defaults to input file with \".pdf\" suffix.""");
             System.exit(0);
         }
         String inputFile = args[0];
+        String outputFile = inputFile + ".pdf";
+        if (args.length > 1) {
+            outputFile = args[1];
+        }
 
-        DocumentConverter converter = DocumentConverterFactory.getConverterForDocument(inputFile, inputFile + ".pdf");
+        DocumentConverter converter = DocumentConverterFactory.getConverterForDocument(inputFile, outputFile);
         if (converter != null) {
             converter.convert();
         } else {
