191a923c7dde47875bdc1b04bc019a6db1dd76dc
Jacobo Aragunde Pérez <jaragunde@igalia.com>
2025-04-10 13:30:05 +0200

    Load all the ttf fonts in the system.
    Loading from hard-coded path /usr/share/fonts.

--- lib/src/main/java/com/igalia/ooxmlexporter/PptxConverter.java
+++ lib/src/main/java/com/igalia/ooxmlexporter/PptxConverter.java
@@ -1,6 +1,7 @@
 package com.igalia.ooxmlexporter;
 
 import de.rototor.pdfbox.graphics2d.PdfBoxGraphics2D;
+import de.rototor.pdfbox.graphics2d.PdfBoxGraphics2DFontTextDrawer;
 import de.rototor.pdfbox.graphics2d.PdfBoxGraphics2DFontTextDrawerDefaultFonts;
 
 import java.io.File;
@@ -19,9 +20,28 @@ public class PptxConverter implements DocumentConverter {
     private String inputFile;
     private String outputFile;
 
+    private static final String SYSTEM_FONT_PATH = "/usr/share/fonts";
+
     public PptxConverter(String inputFile, String outputFile) {
         this.inputFile = inputFile;
         this.outputFile = outputFile;
+    }
+
+    private PdfBoxGraphics2DFontTextDrawer createFontTextDrawer() {
+        PdfBoxGraphics2DFontTextDrawerDefaultFonts fontTextDrawer = new PdfBoxGraphics2DFontTextDrawerDefaultFonts();
+        addFontsToDrawerRecursively(new File(SYSTEM_FONT_PATH), fontTextDrawer);
+        return fontTextDrawer;
+    }
+
+    private void addFontsToDrawerRecursively(File file, PdfBoxGraphics2DFontTextDrawer fontTextDrawer) {
+        if (file.isFile() && file.getName().endsWith(".ttf")) {
+            fontTextDrawer.registerFont(file);
+        }
+        if (file.isDirectory()) {
+            for (File innerFile : file.listFiles()) {
+                addFontsToDrawerRecursively(innerFile, fontTextDrawer);
+            }
+        }
     }
 
     public void convert() {
@@ -34,8 +54,7 @@ public class PptxConverter implements DocumentConverter {
                 document.addPage(page);
                 PdfBoxGraphics2D pdfBoxGraphics2D = new PdfBoxGraphics2D(document, ppt.getPageSize().width, ppt.getPageSize().height);
 
-                PdfBoxGraphics2DFontTextDrawerDefaultFonts fontTextDrawer = new PdfBoxGraphics2DFontTextDrawerDefaultFonts();
-                pdfBoxGraphics2D.setFontTextDrawer(fontTextDrawer);
+                pdfBoxGraphics2D.setFontTextDrawer(createFontTextDrawer());
                 slide.draw(pdfBoxGraphics2D);
                 pdfBoxGraphics2D.dispose();
                 PDPageContentStream contentStream = new PDPageContentStream(document, page);
